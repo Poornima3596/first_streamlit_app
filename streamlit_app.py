71d6764e0c352ab5a48d91c139c45dec30fe5f37
Poornima3596 <134509689+Poornima3596@users.noreply.github.com>
2023-05-27 18:38:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ streamlit.title("hello")
 streamlit.subheader("breakfast options")
 streamlit.text("tea")
 streamlit.text("coffee")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
